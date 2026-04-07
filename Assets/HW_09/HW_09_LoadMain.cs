22403bfe3ade955950f332921115a232f5dc0b16
해나 <71058551+hannah0125@users.noreply.github.com>
2026-04-07 14:53:14 +0900

    change folder name

--- Assets/HW_09/HW_09_LoadMain.cs
+++ Assets/HW_09/HW_09_LoadMain.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class A09_LoadMain : MonoBehaviour
+public class HW_09_LoadMain : MonoBehaviour
 {
     public string SceneName;
 
